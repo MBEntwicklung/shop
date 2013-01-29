89c6bc890efe5a330c5c0fe188d5b828917a1d9d
Marc Bellmann <marc.bellmann@mb-entwicklung.de>
2013-01-29 20:55:38 +0100

    spring config, eclipseLink and spring data jpa reverted

--- shop-demo/src/main/webapp/VAADIN/widgetsets/de.mbentwicklung.shop.AppWidgetSet/deferredjs/C47AAFC16B0A6B7347E820F5A5CABEE2/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.de_mbentwicklung_shop_AppWidgetSet.runAsyncCallback1('Wrb(2381,1,mLe);_.Wb=function iud(){PXb((!KXb&&(KXb=new RXb),KXb),this.a.d)};cMe(lh)(1);\n//@ sourceURL=1.js\n')
--- shop-demo/src/main/webapp/VAADIN/widgetsets/de.mbentwicklung.shop.AppWidgetSet/deferredjs/E742A561A85DC809D5CF55691BE8149D/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.de_mbentwicklung_shop_AppWidgetSet.runAsyncCallback1('xsb(2386,1,$Le);_._b=function Uud(){zYb((!uYb&&(uYb=new BYb),uYb),this.b.e)};PMe(xh)(1);\n//@ sourceURL=1.js\n')
