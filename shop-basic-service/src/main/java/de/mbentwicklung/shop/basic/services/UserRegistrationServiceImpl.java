4c7dfa73d2fea85c2523e305f1c9f4203f8d9a18
Marc Bellmann <marc.bellmann@mb-entwicklung.de>
2013-01-30 20:39:43 +0100

    Spring and Vaadin

--- shop-basic-service/src/main/java/de/mbentwicklung/shop/basic/services/UserRegistrationServiceImpl.java
+++ shop-basic-service/src/main/java/de/mbentwicklung/shop/basic/services/UserRegistrationServiceImpl.java
@@ -1,12 +1,21 @@
 package de.mbentwicklung.shop.basic.services;
 
+import org.springframework.beans.factory.annotation.Required;
+
 import de.mbentwicklung.shop.basic.entities.User;
+import de.mbentwicklung.shop.basic.repositories.AddressRepository;
 
 public class UserRegistrationServiceImpl implements UserRegistrationService {
 
+	private AddressRepository addressRepository;
+	
 	public User register() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
+	
+	@Required
+	public void setAddressRepository(AddressRepository addressRepository) {
+		this.addressRepository = addressRepository;
+	}
 }
--- shop-basic-web/src/main/java/de/mbentwicklung/shop/ApplicationUI.java
+++ shop-basic-web/src/main/java/de/mbentwicklung/shop/ApplicationUI.java
@@ -1,5 +1,7 @@
 package de.mbentwicklung.shop;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -7,15 +9,20 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+import de.mbentwicklung.shop.basic.services.ProductOverviewService;
+
 public class ApplicationUI extends UI {
 
+	@Autowired
+	private ProductOverviewService productOverviewService;
+	
 	@Override
 	protected void init(VaadinRequest request) {
 		final VerticalLayout layout = new VerticalLayout();
 		layout.setMargin(true);
 		setContent(layout);
 
-		Button button = new Button("Click Me");
+		Button button = new Button(this.productOverviewService.getProducts());
 		button.addClickListener(new Button.ClickListener() {
 
 			public void buttonClick(ClickEvent event) {
